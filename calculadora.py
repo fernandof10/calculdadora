c3ae3ac21f8568c3738e4dc19b68c93e55e7aa5a
fernando <fernandhodho@gmail.com>
2019-05-06 20:59:05 -0300

    Layout - frames width equal window width
    - Changed the frames width to be the same as windown width

--- calculadora.py
+++ calculadora.py
@@ -2,7 +2,7 @@
 from tkinter import *
 
 
-#TODO:1 - Crie uma janela, com o nome Calculadora
+#TODO 1: Crie uma janela, com o nome Calculadora
 janela = Tk()
 janela.title("Calculadora")
 
@@ -11,11 +11,20 @@ altura = 250
 tamanho = '{}x{}'.format(largura, altura)
 janela.geometry(tamanho)
 
-#TODO:2 - Crie 2 frames. 1, que contem o resultado e o outro com os botões.
-frame_res = Frame(janela)
+#TODO 2: Crie 2 frames. 1, que contem o resultado e o outro com os botões.
+#TODO 3: O primeiro frame ocupa todo o espaço disponível em X e tem uma borda do tipo afundada
+frame_res = Frame(janela, relief=SUNKEN,  width=largura)
 frame_res.pack()
-frame_but = Frame(janela)
+frame_but = Frame(janela, width=largura)
 frame_but.pack()
 
 #Exibe a janela e faz com que ela fique em loop
 janela.mainloop()
+
+
+
+
+
+#Links legais
+#https://www.tutorialspoint.com/python3/tk_frame.htm
+#https://www.tutorialspoint.com/python/tk_relief.htm
