fbb2338b84a34602b96f84aef11eae321d4aeb61
fernando <fernandhodho@gmail.com>
2019-05-06 21:10:51 -0300

    Layout - Added buttons
    - Added the buttons (0, 1, 2 ... 9, +, -, = ...)
    - Buttons could be created using a for loop. This could reduce the a lot
    the number of lines. However, it is more didatic to explain this
    big code first and latter explain the more pythonic version

--- calculadora.py
+++ calculadora.py
@@ -28,6 +28,46 @@ label_ope.pack()
 label_res = Label(frame_res, text='0',  font=("Helvetica", "18"), width=largura, anchor=E)
 label_res.pack()
 
+#TODO 6: Dentro do segundo frame principal estão os botões. Eles são dispostos em uma grade, como uma tabela.
+b7 = Button(frame_but, text='7')
+b7.grid(row=0, column=0)
+b8 = Button(frame_but, text='8')
+b8.grid(row=0, column=1)
+b9 = Button(frame_but, text='9')
+b9.grid(row=0, column=2)
+bdiv = Button(frame_but, text='\u00F7')
+bdiv.grid(row=0, column=3)
+bac = Button(frame_but, text='AC')
+bac.grid(row=0, column=4)
+
+b4 = Button(frame_but, text='4')
+b4.grid(row=1, column=0)
+b5 = Button(frame_but, text='5')
+b5.grid(row=1, column=1)
+b6 = Button(frame_but, text='6')
+b6.grid(row=1, column=2)
+bmult = Button(frame_but, text='\u00D7')
+bmult.grid(row=1, column=3)
+bapag = Button(frame_but, text='\u2190')
+bapag.grid(row=1, column=4)
+
+b1 = Button(frame_but, text='1')
+b1.grid(row=2, column=0)
+b2 = Button(frame_but, text='2')
+b2.grid(row=2, column=1)
+b3 = Button(frame_but, text='3')
+b3.grid(row=2, column=2)
+bneg = Button(frame_but, text='-')
+bneg.grid(row=2, column=3)
+bres = Button(frame_but, text='=')
+bres.grid(row=2, column=4)
+b0 = Button(frame_but, text='0')
+b0.grid(row=3, column=0)
+bp = Button(frame_but, text='.')
+bp.grid(row=3, column=2)
+bm = Button(frame_but, text='+')
+bm.grid(row=3, column=3)
+
 #Exibe a janela e faz com que ela fique em loop
 
 janela.mainloop()
@@ -42,3 +82,4 @@ janela.mainloop()
 #4 - https://www.tutorialspoint.com/python3/tk_label.htm
 #4 - https://www.tutorialspoint.com/python/tk_fonts.htm
 #5 - https://www.tutorialspoint.com/python/tk_anchors.htm
+#6 - https://www.fileformat.info/info/unicode/category/Sm/list.htm
